2620baaa148cba3faec4b54329a5c6ace1d7fa74
Hoseop.kim <kimho314@naver.com>
2025-05-20 18:08:31 +0300

    [feat]: Add examples of local classes

---
+++ src/nested/local/LocalOuterV4.java
@@ -0,0 +1,35 @@
+package nested.local;
+
+public class LocalOuterV4 {
+
+    private int outInstanceVar = 3;
+
+    public Printer process(int paramVar) {
+        int localVar = 1;
+
+        class LocalPrinter implements Printer {
+
+            int value = 0;
+
+            @Override
+            public void print() {
+                System.out.println("value=" + value);
+                System.out.println("localVar=" + localVar);
+                System.out.println("paramVar=" + paramVar);
+                System.out.println("outInstanceVar=" + outInstanceVar);
+            }
+        }
+
+        Printer printer = new LocalPrinter();
+        // 만약 localVar의 값을 변경한다면? 다시 캡처해야 하나??
+        // localVar = 10; // 컴파일 오류
+        // paramVar = 20; // 컴파일 오류
+        return printer;
+    }
+
+    public static void main(String[] args) {
+        LocalOuterV4 localOuter = new LocalOuterV4();
+        Printer printer = localOuter.process(2);
+        printer.print();
+    }
+}
