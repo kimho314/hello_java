377fd1db1528de6215d33c7aa0adb6e72e6388af
Hoseop.Kim <kimho314@gmail.com>
2025-05-29 11:17:24 +0300

    [feat]: Add synchronized BankAccountV2 and BankAccountV3 implementations for thread-safe transactions

--- src/thread/sync/BankMain.java
+++ src/thread/sync/BankMain.java
@@ -6,7 +6,10 @@ import static util.ThreadUtils.sleep;
 public class BankMain {
 
     public static void main(String[] args) throws InterruptedException {
-        BankAccount account = new BankAccountV1(1000);
+//        BankAccount account = new BankAccountV1(1000);
+//        BankAccount account = new BankAccountV2(1000);
+        BankAccount account = new BankAccountV3(1000);
+
         Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
         Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
         t1.start();
@@ -16,6 +19,7 @@ public class BankMain {
         log("t2 state: " + t2.getState());
         t1.join();
         t2.join();
+
         log("최종 잔액: " + account.getBalance());
     }
 }
